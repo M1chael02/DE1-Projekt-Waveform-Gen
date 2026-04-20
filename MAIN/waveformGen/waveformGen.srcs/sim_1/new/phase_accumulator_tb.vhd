12897c6610445e77c5072a748d01e94e0cc25102
M1chael02.com <michaeljurca1@gmail.com>
2026-04-20 17:08:52 +0200

    added phase_accumulator_tb

---
+++ MAIN/waveformGen/waveformGen.srcs/sim_1/new/phase_accumulator_tb.vhd
@@ -0,0 +1,82 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 15.04.2026 23:45:00
+-- Design Name: 
+-- Module Name: phase_accumulator_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity phase_accumulator_tb is
+end entity;
+
+architecture tb of phase_accumulator_tb is
+
+    -- DUT signály
+    signal clk        : std_logic := '0';
+    signal rst        : std_logic := '1';
+    signal phase_step : std_logic_vector(31 downto 0) := (others => '0');
+    signal phase_out  : std_logic_vector(31 downto 0);
+
+    -- clock period
+    constant CLK_PERIOD : time := 10 ns; -- 100 MHz
+
+begin
+
+    -- instance DUT
+    uut: entity work.phase_accumulator
+        port map (
+            clk        => clk,
+            rst        => rst,
+            phase_step => phase_step,
+            phase_out  => phase_out
+        );
+
+    -- clock generation
+    clk_process : process
+    begin
+        while true loop
+            clk <= '0';
+            wait for CLK_PERIOD/2;
+            clk <= '1';
+            wait for CLK_PERIOD/2;
+        end loop;
+    end process;
+
+    -- stimulus
+    stim_proc : process
+    begin
+
+        -- reset
+        wait for 50 ns;
+        rst <= '0';
+
+        -- zapnutí
+
+        -- test 1: malé phase_step
+        phase_step <= std_logic_vector(to_unsigned(1000, 32));
+        wait for 200 ns;
+
+        -- test 2: větší krok (rychlejší změna)
+        phase_step <= std_logic_vector(to_unsigned(100000, 32));
+        -- konec simulace
+        wait;
+    end process;
+
+end architecture;
