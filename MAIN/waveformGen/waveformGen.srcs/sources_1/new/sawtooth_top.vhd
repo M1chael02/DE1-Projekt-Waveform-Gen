2781b702e222309523976dcd03820a80a8aff1bf
M1chael02.com <michaeljurca1@gmail.com>
2026-04-20 17:18:52 +0200

    added top level sawtooth_top.vhd for sawtooth generator

---
+++ MAIN/waveformGen/waveformGen.srcs/sources_1/new/sawtooth_top.vhd
@@ -0,0 +1,97 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 17.04.2026 02:40:13
+-- Design Name: 
+-- Module Name: sawtooth_top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity sawtooth_top is
+    Port ( clk          : in STD_LOGIC;
+           rst          : in STD_LOGIC;
+           phase_Shift  : in STD_LOGIC_VECTOR (31 downto 0);
+           output_Saw   : out STD_LOGIC);
+end sawtooth_top;
+
+architecture Behavioral of sawtooth_top is
+    -- Component declaration for Phase Accumulator
+    component phase_accumulator is
+        port (
+            clk         : in  std_logic;            
+            rst         : in  std_logic;
+            phase_step  : in  std_logic_vector(31 downto 0); 
+            phase_out   : out std_logic_vector(31 downto 0)
+        );
+    end component phase_accumulator;
+    
+    
+     -- Component declaration for sawtooth generator
+    component sawtoothGen is
+        port (
+            phase_in   : in  std_logic_vector(31 downto 0);
+            sawtooth   : out std_logic_vector(7 downto 0)
+        );
+    end component sawtoothGen;
+    
+         -- Component declaration for sawtooth generator
+    component sigma_Delta is
+        port(
+            clk         : in  std_logic;
+            rst         : in  std_logic;
+            data_in     : in  std_logic_vector(7 downto 0);
+            dac_out     : out std_logic
+        );
+    end component sigma_Delta;  
+    
+    signal s_phase  : std_logic_vector(31 downto 0) := (others => '0');
+    signal s_saw    : std_logic_vector(7 downto 0)  := (others => '0');
+
+begin
+    
+    phase_acc_2 : phase_accumulator
+        port map (
+            clk => clk,
+            rst => rst,
+            phase_step => phase_Shift,
+            phase_out => s_phase
+        );
+        
+    sawtooth_gen : sawtoothGen
+        port map (
+            phase_in => s_phase,
+            sawtooth => s_saw
+        );
+        
+    sigma_delta_0: sigma_Delta
+        port map (
+            clk => clk,
+            rst => rst,
+            data_in => s_saw,
+            dac_out => output_Saw
+        );
+end Behavioral;
