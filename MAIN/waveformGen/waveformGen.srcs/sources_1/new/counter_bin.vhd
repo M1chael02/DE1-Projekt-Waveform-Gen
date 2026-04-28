d71b021bdaa0b9915f5e299cb16aca43add9f425
nadvornikdominik <145449627+nadvornikdominik@users.noreply.github.com>
2026-04-28 09:07:24 +0200

    Update counter_bin.vhd

--- MAIN/waveformGen/waveformGen.srcs/sources_1/new/counter_bin.vhd
+++ MAIN/waveformGen/waveformGen.srcs/sources_1/new/counter_bin.vhd
@@ -3,6 +3,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity counter_bin is
+    -- G_BITS determines how many will counter count up to
     generic ( G_BITS : positive := 3);
     Port (
         clk : in STD_LOGIC;
@@ -14,18 +15,22 @@ end counter_bin;
 
 architecture Behavioral of counter_bin is
 
+    -- signal that will store amount counter counted
     signal sig_cnt : integer range 0 to (2**G_BITS - 1) := 0;
 
 begin
 
     process (clk)
     begin
+        --reacts to clock and en input from clk_en component to ensure right refresh rate
         if (rising_edge(clk) and en = '1') then
             if rst = '1' then
                 sig_cnt <= 0;
             elsif sig_cnt = (2**G_BITS - 1) then
+                -- sets sig_cnt back to zero when it reaches max value 
                 sig_cnt <= 0;
             else
+                -- otherwise adds one to sig_cnt
                 sig_cnt <= sig_cnt + 1;
             end if;
         end if;
