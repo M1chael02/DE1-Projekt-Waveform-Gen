75ebdcab8c9967a609d614b92d59e6aac494aebc
nadvornikdominik <145449627+nadvornikdominik@users.noreply.github.com>
2026-04-28 09:13:50 +0200

    Update counter_blink.vhd

--- MAIN/waveformGen/waveformGen.srcs/sources_1/new/counter_blink.vhd
+++ MAIN/waveformGen/waveformGen.srcs/sources_1/new/counter_blink.vhd
@@ -2,7 +2,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity counter_blink is
-    generic ( G_MAX : positive := 5 );  -- Default number of clock cycles
+    -- Default number of clock cycles
+    generic ( G_MAX : positive := 5 );  
     Port ( clk : in STD_LOGIC;
            rst : in STD_LOGIC;
            dis : in STD_LOGIC;
@@ -14,25 +15,27 @@ architecture Behavioral of counter_blink is
     signal sig_cnt : integer range 0 to G_MAX-1;
 
 begin
-
-    -- Count clock pulses and generate a one-clock-cycle enable pulse
     process (clk) is
     begin
-        if rising_edge(clk) then  -- Synchronous process
-            if rst = '1' then     -- High-active reset
-                ce      <= '0';   -- Reset output
-                sig_cnt <= 0;     -- Reset internal counter
-
-            elsif dis = '1' then
+        -- Reacts to clock
+        if rising_edge(clk) then  
+            if rst = '1' then     
+                ce      <= '0';   
+                sig_cnt <= 0;     
+            -- Dissables blinking when frequency isnt changing
+            elsif dis = '0' then
                 ce      <= '0';
                 sig_cnt <= 0'
 
+            -- Returns counter to 0 when it reaches max
             elsif sig_cnt = G_MAX-1 then
                 ce      <= '1';
                 sig_cnt <= 0;
+            -- Counter outputs 1 when sig_cnt over half of max value
             elsif sig_cnt > G_MAX/2 then
                 ce      <= '1';
                 sig_cnt <= sig_cnt + 1;
+            -- Counter outputs 0 when sig_cnt under half of max value
             elsif sig_cnt < G_MAX/2 then
                 ce      <= '0';
                 sig_cnt <= sig_cnt + 1;
@@ -40,8 +43,8 @@ begin
                 ce      <= '0';
                 sig_cnt <= sig_cnt + 1;
 
-            end if;  -- End if for reset/check
-        end if;      -- End if for rising_edge
+            end if;  
+        end if;      
     end process;
 
 end Behavioral;
